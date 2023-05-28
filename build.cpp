1dab6d9cb930b0b41642db7781d8b232b44cceb5
Eduardo Costa <m4c0@users.noreply.github.com>
2023-05-28 11:14:46 +0100

    base module

--- build.cpp
+++ build.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
   using namespace ecow;
 
-  auto all = unit::create<seq>("all");
-  all->add_unit<>("poc");
-  return run_main(all, argc, argv);
+  auto m = unit::create<mod>("rng");
+
+  return run_main(m, argc, argv);
 }
